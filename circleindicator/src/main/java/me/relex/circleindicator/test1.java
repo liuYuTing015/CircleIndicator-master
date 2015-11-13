9a0ca64ae6dec407fe8d85469b9d159ea1acc272
liuYuTing015 <963099134@qq.cm>
2015-11-13 12:30:00 +0800

    test1

---
+++ circleindicator/src/main/java/me/relex/circleindicator/test1.java
@@ -0,0 +1,12 @@
+package me.relex.circleindicator;
+
+/**
+ * Created by vaio on 2015/11/13.
+ */
+public class test1 {
+    private  boolean add;
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+}
